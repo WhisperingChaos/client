77bce4e4790b760d58f25528999edc940421210d
Rich Moyse <rich@moyse.us>
2017-06-27 10:28:03 -0400

    incorporate docker golang API client. configure from either ENVs or config file.

---
+++ docker/client.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+	cttp "imagepvdr/client/http"
+	"net/http"
+
+	dkr "github.com/moby/moby/client"
+)
+
+type Opts struct {
+	TmOut      cttp.Opts
+	TLS        cttp.TLSclientOpts
+	RootURL    string
+	APIversion string
+}
+
+func Config(opts Opts) (cli *dkr.Client, err error) {
+	if opts.RootURL != "" {
+		// config file overrides env variables
+		hClient := new(http.Client)
+		if err = cttp.Config(opts.TmOut, opts.TLS, hClient); err != nil {
+			return
+		}
+		var headers map[string]string
+		return dkr.NewClient(opts.RootURL, opts.APIversion, hClient, headers)
+	}
+	return dkr.NewEnvClient()
+}
